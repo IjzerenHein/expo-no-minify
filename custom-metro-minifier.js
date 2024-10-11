19014d6323a1992c78fa2006044f72b500c38c20
Hein Rutjes <hrutjes@gmail.com>
2024-10-11 10:36:16 +0200

    Explicitely enable hermes

--- custom-metro-minifier.js
+++ custom-metro-minifier.js
@@ -2,6 +2,7 @@ module.exports = async (code, map, reserved, config) => {
   console.log("custom-metro-minifier.js is called");
 
   // Return empty bundle to prove this isn't called when hermes is enabled
+  // When switching to jsc, this will be called and the app fails to load with an error (as expected)
   return {
     code: "",
     map: [],
