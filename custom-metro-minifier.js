54ea5b00bcdb04f2747c942e09731f4365a10606
Hein Rutjes <hrutjes@gmail.com>
2024-10-11 10:06:33 +0200

    expo prebuild

--- custom-metro-minifier.js
+++ custom-metro-minifier.js
@@ -1,7 +1,7 @@
 module.exports = async (code, map, reserved, config) => {
-  console.log("Minifier should be called, but isnt");
+  console.log("custom-metro-minifier.js is called");
 
-  // Return empty bundle to prove this isn't called
+  // Return empty bundle to prove this isn't called when hermes is enabled
   return {
     code: "",
     map: [],
